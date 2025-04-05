dc63c5cd4d65f2cf6fe2cbe2b3eef0a832194621
JIgor-Silva <joao.igor@thinkor.com.br>
2025-04-05 02:42:01 -0300

    fix/ajuste da propriedade zp_code na model adress

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,9 +2,9 @@ class Address < ApplicationRecord
   belongs_to :person
 
   validates :street, :city, :state, presence: true
-  validates :zip_code, format: { with: /\A\d{5}(-\d{4})?\z/, message: "deve ser no formato 12345 ou 12345-6789" }
+  validates :zp_code, format: { with: /\A\d{5}(-\d{4})?\z/, message: "deve ser no formato 12345 ou 12345-6789" }
 
   def full_address
-    "#{street}, #{city}, #{state} #{zip_code}"
+    "#{street}, #{city}, #{state} #{zp_code}"
   end
 end
