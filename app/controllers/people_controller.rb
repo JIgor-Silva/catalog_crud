d52926f456efd939571762a259d77b903f29bb9d
JIgor-Silva <joao.igor@thinkor.com.br>
2025-04-05 12:01:53 -0300

    fix/correção controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,44 +1,68 @@
-class PeoplesController < ApplicationController
-  before_action :set_person, only: [ :show, :edit, :update, :destroy ]
+class PeopleController < ApplicationController
+  before_action :set_person, only: [:show, :edit, :update, :destroy]
 
+  # GET /people
   def index
-    @persons = Person.all.includes(:phone_numbers, :address)
+    @people = Person.all.includes(:phone_numbers, :address)
+    respond_to do |format|
+      format.html  # ← isso permite responder a .html
+      format.json { render json: @people }
+    end
   end
 
+  # GET /people/1
   def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @person }
+    end
   end
 
+  # GET /people/new
   def new
     @person = Person.new
     @person.phone_numbers.build
     @person.build_address
+    respond_to do |format|
+      format.html
+      format.json { render json: @person }
+    end
   end
 
+  # GET /people/1/edit
   def edit
     @person.phone_numbers.build if @person.phone_numbers.empty?
     @person.build_address if @person.address.nil?
+    respond_to do |format|
+      format.html
+      format.json { render json: @person }
+    end
   end
 
+  # POST /people
   def create
     @person = Person.new(person_params)
+    
     if @person.save
-      redirect_to people_path(@person), notice: "Pessoa criada com sucesso."
+      redirect_to person_path(@person), notice: "Pessoa criada com sucesso."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  # PATCH/PUT /people/1
   def update
     if @person.update(person_params)
-      redirect_to @person, notice: "Pessoa atualizada com sucesso."
+      redirect_to person_path(@person), notice: "Pessoa atualizada com sucesso."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
+  # DELETE /people/1
   def destroy
     @person.destroy
-    redirect_to people_url, notice: " Pessoa removida com sucesso."
+    redirect_to people_url, notice: "Pessoa removida com sucesso."
   end
 
   private
